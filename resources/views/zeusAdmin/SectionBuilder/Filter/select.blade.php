2853a1c600c80760ff7e68448a5946d2d27a7c9a
ID-EAST | Артем Солдатов <arty.sold@gmail.com>
2019-06-22 09:28:55 +0400

    Обновляем Zeus Admin

--- resources/views/zeusAdmin/SectionBuilder/Filter/select.blade.php
+++
@@ -1,6 +0,0 @@
-<select name="filter[{{ $name }}]" data-filter-name="{{ $name }}" class="form-control filter-input">
-        <option value="" selected disabled>Выберите пункт</option>
-    @foreach($options as $key => $option)
-        <option value="{{ $key }}">{{ $option }}</option>
-    @endforeach
-</select>
--- resources/views/zeusAdmin/SectionBuilder/Form/Fields/dropZone.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="form-group">
-    <label for="input_{{ $name }}">{{ $label }} </label>
-    <div class="dropzone" id="{{ $id }}" data-dropzone-url="{{ $url }}"></div>
-</div>
