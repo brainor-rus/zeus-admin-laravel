1a35adc27e2fb1729d90f42a93e1bc5b1862f56e
ID-EAST | Артем Солдатов <arty.sold@gmail.com>
2019-07-08 18:41:51 +0400

    Обновляем Zeus Admin. Добавляем пример для поля формы "Галлерия".

--- app/Models/City.php
+++ app/Models/City.php
@@ -3,10 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Zeus\Admin\Cms\Models\ZeusAdminFile;
 
 class City extends Model
 {
     protected $fillable = [
-        'name', 'slug'
+        'name',
     ];
+
+    public function zaGalleryImages() {
+        return $this->morphToMany(ZeusAdminFile::class, 'zeus_admin_fileble');
+    }
 }
